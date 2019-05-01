63e8efa0e16edfa5ddbe8d75e7139a963af4c3ca
Josh Justice <me@codingitwrong.com>
2019-04-30 20:14:02 -0400

    Hard coded widget endpoint

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 class WidgetsController < ApiController
+  def index
+    render json: sample_widgets
+  end
+
+  private
+
+  def sample_widgets
+    [
+      Widget.new(name: 'Widget 1'),
+      Widget.new(name: 'Widget 2'),
+    ]
+  end
 end
