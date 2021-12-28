48b2b2ad4816f8d91e06af9e63f67daf4174f09a
Josh Justice <me@codingitwrong.com>
2021-12-28 07:15:02 -0500

    Add multiple widgets for user

--- spec/requests/widgets_spec.rb
+++ spec/requests/widgets_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 
 RSpec.describe 'widgets', type: :request do
   let!(:user) { FactoryBot.create(:user) }
-  let!(:widget) { FactoryBot.create(:widget, user: user) }
+  let!(:user_widget1) { FactoryBot.create(:widget, user: user) }
+  let!(:user_widget2) { FactoryBot.create(:widget, user: user) }
   let!(:other_widget) { FactoryBot.create(:widget) }
   let(:token) do
     FactoryBot.create(:access_token, resource_owner_id: user.id).token
@@ -21,8 +22,9 @@ RSpec.describe 'widgets', type: :request do
 
       widgets = JSON.parse(response.body)
 
-      expect(widgets.length).to eq(1)
-      expect(widgets[0]['name']).to eq(widget.name)
+      expect(widgets.length).to eq(2)
+      expect(widgets[0]['name']).to eq(user_widget1.name)
+      expect(widgets[1]['name']).to eq(user_widget2.name)
     end
   end
 end
