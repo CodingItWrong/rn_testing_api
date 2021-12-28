4ce1836f445a63316a747625a1a6daa04e829cac
Josh Justice <me@codingitwrong.com>
2021-12-28 07:13:44 -0500

    Extract headers for reuse

--- spec/requests/list_widgets_spec.rb
+++ spec/requests/list_widgets_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe 'list widgets', type: :request do
   let!(:user) { FactoryBot.create(:user) }
   let!(:widget) { FactoryBot.create(:widget, user: user) }
   let!(:other_widget) { FactoryBot.create(:widget) }
-
   let(:token) do
     FactoryBot.create(:access_token, resource_owner_id: user.id).token
+  end
+  let(:headers) do
+    { 'Authorization' => "Bearer #{token}" }
   end
 
   context 'when authenticated' do
     it "returns user's widgets" do
-      headers = { 'Authorization' => "Bearer #{token}" }
-
       get '/widgets', headers: headers
 
       expect(response).to be_successful
